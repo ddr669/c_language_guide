c1c7b0c063e5836ffe72c407494504df2c6d19e3
ddr669 <dudu.herique.x@gmail.com>
2025-07-22 14:53:50 -0300

    c printf format %

---
+++ basics/c_format_porc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(int args, char **argv[]){
+	int var = 0;
+	printf("number in the variable: %d\n", var); // %d is for double
+	char char_o = 'o';
+	printf("a char in the variable: %c\n", char_o); // %c is for char
+
+}
