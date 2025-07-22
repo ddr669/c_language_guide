7133e31bd4f0e2ff2dea784222384cd5a94e22a3
ddr669 <dudu.herique.x@gmail.com>
2025-07-22 14:47:01 -0300

    hello world in c

--- basics/hello_world_inC.c
+++ basics/hello_world_inC.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+/*
+	This is a hello, world in c
 
-
-
+*/
 
 int main() {
-	printf("Hello, World!");
-	return 0;
+	printf("Hello, World!"); // this is a built in method in stdio.h
+	return 0; // - keyword return
 }
 
 
