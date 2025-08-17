599dd62df8002f50eb7d909f0d17e1ace9cf0abc
ddr669 <dudu.herique.x@gmail.com>
2025-08-17 18:26:14 -0300

    add bubble sort method

--- data_structures/binary_search.c
+++ data_structures/binary_search.c
@@ -17,9 +17,7 @@ void showTime(struct BinaryTree *array, int length, int low)
 {
 	void sleep_ms(int mil);
 	for(int i=low; i!= length; i++){
-		sleep_ms(1);
-		for(int a=0;a!= 9; a++){
-			sleep_ms(1);
+		for(int a=0;a!= 2; a++){
 			printf("%d", a);
 			fflush(stdout);
 			sleep_ms(1);
@@ -31,15 +29,76 @@ void showTime(struct BinaryTree *array, int length, int low)
 	}
 	printf("\n");
 }
-
-void bubbleSort(struct BinaryTree *array, int length, int ini_value, int method){
+void showBubble(struct BinaryTree *array, int length,int pos_colored, int red_colored){
+	void sleep_ms(int mil);
+	for(int i=0; i!= length; i++){
+		if (i == pos_colored){
+			printf("\033[0;32m %d \033[0m", array->array[i]);
+		}else if(i == red_colored){
+			printf("\033[0;31m %d \033[0m", array->array[i]);
+		}else{
+			printf(" %d ", array->array[i]);
+			fflush(stdout);
+		}
+		sleep_ms(2);
+	}
+	sleep_ms(2);
+	printf("\n");
+}
+void bubbleSort(struct BinaryTree *array, int length){
 	// ini_value = array.array[0] whatever
 	// method = 0 | 1 - 0 to pass through array by 0 : or on the contrary
 	int arraysize = length;
-	int array_value = array->array[ini_value];
 	int compare_check = 0;
-	int last_value;
+	int pivot = 0;
+	int last_value = 0;
+	int red_value = arraysize;
+	int temp_value;
+	int right = 0;
 	showTime(array, arraysize, 0);
+	while(compare_check != arraysize){
+		showBubble(array, arraysize, last_value, red_value);
+		if (right == 0 && pivot != arraysize){
+			pivot = pivot + 1;
+			red_value = red_value - 1;
+			if (array->array[last_value] > array->array[pivot]){
+				temp_value = array->array[pivot];
+				array->array[pivot] = array->array[last_value];
+				array->array[last_value] = temp_value;
+				compare_check = 0;
+			}else {
+				compare_check = compare_check + 1;
+			}
+			last_value = last_value + 1;	
+		}else if (right == 1 && pivot != 0){
+			pivot = pivot -1;
+			red_value = red_value + 1;
+			if (array->array[last_value] < array->array[pivot]){
+				temp_value = array->array[pivot];
+				array->array[pivot] = array->array[last_value];
+				array->array[last_value] = temp_value;
+				compare_check = 0;
+
+			}else {
+				compare_check = compare_check + 1;
+			}
+			last_value = last_value - 1;
+		}else if(pivot == arraysize && right == 0){
+			right = 1;
+			temp_value = pivot;
+			pivot = last_value;
+			last_value = temp_value;
+			compare_check = 0;
+			red_value = 0;
+		}else if(pivot == 0 && right == 1){
+			right = 0;
+			temp_value = pivot;
+			pivot = last_value;
+			last_value = temp_value;
+			compare_check = 0;
+			red_value = arraysize;
+		}
+	}
 	
 }
 
@@ -146,32 +205,34 @@ void getArray(struct BinaryTree *array){
 };
 
 int main(int args, char **argv[]){
-	char argvs_[10];
+
 	struct BinaryTree array;
 	void sortTree(struct BinaryTree*, int length);
 	void showArray(struct BinaryTree);
 	void getArray(struct BinaryTree*);
-	void bubbleSort(struct BinaryTree*, int length, int ini_value, int method);
+	void bubbleSort(struct BinaryTree*, int length);
 	int binarySearch(struct BinaryTree*, int target, int low, int high);
 	int arraysize = ARRAYSIZE;
+
 	srand(time(NULL));
 	getArray(&array);
-	// sort to make a BinarySearch
-	/*sortTree(&array, arraysize);
-	printf("\n");
-	printf("BinarySearch->\n");
-	int some_number = 25;
-	int resultado = binarySearch(&array, some_number, 0, arraysize);
-	if(resultado == -1){
-		printf("The value %d is not in %d#~:array", some_number, &array);
-	}else{
-		printf("value %d in cell %d\n", some_number ,resultado);
-	}*/
-	bubbleSort(&array, arraysize, 0, 0);
 
+/////////////////////////////////////////////// 
+///	BinarySearch with quicksort:
+//	sort to make a BinarySearch
+//	sortTree(&array, arraysize);
+//	printf("\n");
+//	printf("BinarySearch->\n");
+//	int some_number = 25;
+//	int resultado = binarySearch(&array, some_number, 0, arraysize);
+//	if(resultado == -1){
+//		printf("The value %d is not in %d#~:array", some_number, &array);
+//	}else{
+//		printf("value %d in cell %d\n", some_number ,resultado);
+//	}
+//	
+/////////////////////////////////////////////////////
 
-
-
-
+	bubbleSort(&array, arraysize);
 	return 0;
 };
