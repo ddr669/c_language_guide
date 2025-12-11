2afb2e2439b8e4f3c63e218a64dacea3b5df48da
ddr669 <dudu.herique.x@gmail.com>
2025-12-11 16:00:07 -0300

    binary search segregate and moved to src/

---
+++ data_structures/arrays/src/binary_search.c
@@ -0,0 +1,21 @@
+#include "binary_search.h"
+
+int binarySearch(struct ArrayLike *array, int target, int low, int high){
+	if (high-low == 1){
+		return -1;
+	}
+	if (high > low){
+		int mid = low + (high -low) / 2;
+		showTime(array, high, low);
+		if(array->array[mid] == target){
+			return mid;
+		}
+		if(array->array[mid] > target){
+			return binarySearch(array, target, low, mid);
+		}
+		if (array->array[mid] < target){
+			return binarySearch(array, target, mid, high);
+		}
+	}
+	return -1;
+};
---
+++ data_structures/arrays/src/binary_search.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "array_struct.h"
+#include "showTime.h"
+
+#if !defined binarySearch
+int binarySearch(struct ArrayLike *array, int target, int low, int high);
+#endif
