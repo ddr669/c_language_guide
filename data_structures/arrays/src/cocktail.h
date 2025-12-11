e506ef7ef642f5cba3d93872de8211244d602b0c
ddr669 <dudu.herique.x@gmail.com>
2025-12-11 15:54:52 -0300

    renamed and header of removed file bubbleSort.c

---
+++ data_structures/arrays/src/cocktail.h
@@ -0,0 +1,12 @@
+
+#include "array_struct.h"
+#include "showTime.h"
+#include <stdio.h>
+#include "sleep_ms.h"
+
+#ifndef cocktailSort
+void cocktailSort(struct ArrayLike *array, int length);
+#endif
+
+#define TIMESTOP 1
+
---
+++ data_structures/arrays/src/cocktailSort.c
@@ -0,0 +1,93 @@
+#include "cocktail.h"
+/*--------------*
+ *		*
+ *		*
+ *		*
+ *		*
+ *		*
+ *		*
+ *		*
+ *		*
+ *		*
+ *--------------*/
+
+void showBubble(struct ArrayLike *array,
+		int length,
+		int pos_colored,
+		int red_colored){
+	int color_grade = 0;
+	for(int i=0;i!=length;i++){
+		if(i==pos_colored){
+			printf("\033[0;32m %d \033[0m", array->array[i]);
+		}else if(i==red_colored){
+			printf("\033[0;31m %d \033[0m", array->array[i]);
+		}else{
+			color_grade = (30 + 0);
+			printf("\033[0;%dm %d \033[0m",
+					color_grade,
+					array->array[i]);
+			fflush(stdout);
+		}
+		sleep_ms(TIMESTOP);
+	}
+	sleep_ms(TIMESTOP);
+	printf("\n");
+}
+
+
+void cocktailSort(struct ArrayLike *array, int length){
+
+	int arraysize = length;
+	int compare_check = 0;
+	int pivot = 0;
+	int last_value = 0;
+	
+	int red_value = arraysize;
+	int temp_value;
+	int tmp_val;
+	int right = 0;
+	showTime(array, arraysize, 0);
+	while(compare_check != arraysize-1){
+		showBubble(array, arraysize, last_value, red_value);
+		if (right == 0 && pivot < arraysize-1){
+			pivot = pivot + 1;
+			red_value = red_value - 1;
+			if (array->array[last_value] > array->array[pivot]){
+				temp_value = array->array[pivot];
+				array->array[pivot] = array->array[last_value];
+				array->array[last_value] = temp_value;
+				compare_check = 0;
+			}else {
+				compare_check = compare_check + 1;
+			}
+			last_value = last_value + 1;
+		}else if (right == 1 && pivot != 0){
+			pivot = pivot - 1;
+			red_value = red_value + 1;
+			if (array->array[last_value] < array->array[pivot]){
+				temp_value = array->array[pivot];
+				array->array[pivot] = array->array[last_value];
+				array->array[last_value] = temp_value;
+				compare_check = 0;
+			}else {
+				compare_check = compare_check + 1;
+			}
+			last_value = last_value - 1;
+		}else if(pivot == arraysize-1 && right == 0){
+			right = 1;
+			tmp_val = pivot;
+			pivot = last_value;
+			last_value = tmp_val;
+			compare_check = 0;
+			red_value = 0;
+		}else if(pivot == 0 && right == 1){
+			right = 0;
+			tmp_val = pivot;
+			pivot = last_value;
+			last_value = tmp_val;
+			compare_check = 0;
+			red_value = arraysize;
+		}
+	}
+	
+}
