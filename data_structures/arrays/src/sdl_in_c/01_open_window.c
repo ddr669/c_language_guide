cde69942ad9ea49f3018f2a61afa977a6a70452c
ddr669 <dudu.herique.x@gmail.com>
2025-12-15 16:06:08 -0300

    testing gui with sdl3

--- data_structures/arrays/src/sdl_in_c/01_open_window.c
+++ data_structures/arrays/src/sdl_in_c/01_open_window.c
@@ -1,39 +1,51 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_timer.h>
 #include <SDL3/SDL_main.h>
+#include <stdio.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
 /* -------------- */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
-	SDL_SetAppMetadata("Example of a window with SDL3", "1.0", "/x00");
+	SDL_SetAppMetadata("GUI to virtualizate a array sort", "1.0", "/x00");
 	if(!SDL_Init(SDL_INIT_VIDEO)){
 		SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
 		return SDL_APP_FAILURE;
 	} 
-	if(!SDL_CreateWindowAndRenderer("Window with SDL3",720,480, 0,&window, &renderer)){
-		SDL_Log("Couldn't Initialize window: %s", SDL_GetError());
+	if(!SDL_CreateWindowAndRenderer("Array/data_structure",720,480, 0,&window, &renderer)){
+		SDL_Log("Couldn't Initialize window: %s",SDL_GetError());
 		return SDL_APP_FAILURE;
 	}
 	return SDL_APP_CONTINUE;
 }
 /* ----------- */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
+SDL_AppResult SDL_AppEvent(void *appstate,SDL_Event *event){
 	if(event->type == SDL_EVENT_QUIT){
-		return SDL_APP_SUCCESS;
+		return SDL_APP_SUCCESS;}
+	switch(event->type){
+		case SDL_KEYDOWN:
+			printf("%c %d", event->key, event->key);
 	}
-	return SDL_APP_CONTINUE;
-}
+		
+	return SDL_APP_CONTINUE;}
 /* ----------- */
 SDL_AppResult SDL_AppIterate(void *appstate){
+	SDL_Delay(1000/60);
 	const double now = ((double)SDL_GetTicks()) / 1000.0;
-	const float red = (float)  (0.5 + 0.5 * SDL_sin(now));
-	const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3 ));
-	const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3 ));
-	SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+	
+	float red, green, blue;
+	red = 0.25;green = 0.25;blue = 0.25;
+
+	SDL_SetRenderDrawColorFloat(renderer,
+			red,
+			green,
+			blue,
+			SDL_ALPHA_OPAQUE_FLOAT);
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
+
 	return SDL_APP_CONTINUE;
 }
 
